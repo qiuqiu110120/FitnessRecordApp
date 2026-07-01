a9763d2342f8f378081a39ee3a0c6e7d2b922ce4
qiuqiu110120 <qiuqiu110120@users.noreply.github.com>
2026-07-01 23:35:23 +0800

    Bump version to 0.1.24

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.example.fitnessrecord"
         minSdk = 26
         targetSdk = 35
-        versionCode = 24
-        versionName = "0.1.23"
+        versionCode = 25
+        versionName = "0.1.24"
     }
 
     buildTypes {
