a8c832e61e00dfb0e0f9f89988afb4319f874a11
qiuqiu110120 <qiuqiu110120@users.noreply.github.com>
2026-07-16 22:55:41 +0800

    Bump version to 0.1.30

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.example.fitnessrecord"
         minSdk = 26
         targetSdk = 35
-        versionCode = 30
-        versionName = "0.1.29"
+        versionCode = 31
+        versionName = "0.1.30"
     }
 
     buildTypes {
